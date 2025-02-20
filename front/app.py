28de99824728ed0235739e42a6558fbc67747a41
Nicolas Delplanque <nicolas@delplanque.eu>
2025-02-20 22:58:08 +0100

    feat(front): added a streamlit starter

---
+++ front/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+API_V1_URL = "http://127.0.0.1:5000/api/v1"
+
+def v1(endpoint):
+    return f"{API_V1_URL}/{endpoint}"
+
+st.title("Homepedia 🏠")
+
+health = requests.get(v1("health"))
+if health.status_code == 200:
+    st.write("API Flask : Online ✅")
+else:
+    st.error("API Flask : Offline ❌")
+
+if st.button("Fetch data"):
+    response = requests.get(v1("data"))
+    if response.status_code == 200:
+        data = response.json()
+        st.write(f"Message : {data['message']}")
+        st.write(f"Value : {data['value']}")
+    else:
+        st.error("Error fetching data.")

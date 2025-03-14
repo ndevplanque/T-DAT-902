446c4a49259de308dc2530c8688fca4ff8879fd0
Nicolas Delplanque <nicolas@delplanque.eu>
2025-03-14 17:05:38 +0100

    feat(PriceTable): fixed not working

--- frontend/app/components/PriceTable.py
+++ frontend/app/components/PriceTable.py
@@ -2,20 +2,14 @@ import streamlit as st
 import pandas as pd
 
 def PriceTable(data):
-    """ Affiche un tableau interactif des prix immobiliers. """
-    if not data or "price_tables" not in data:
-        st.error("Données invalides ou indisponibles.")
-        return
+    with st.expander(f"📍 {data['title']} (Min: {data['aggs']['min_price']}€ | Max: {data['aggs']['max_price']}€)", expanded=False):
+        # Transformation des données en DataFrame
+        data_list = [{
+            "Zone": zone["name"],
+            "Prix (€/m²)": zone["price"]
+        } for zone in data["items"]]
 
-    for table in data["price_tables"]:
-        with st.expander(f"📍 {table['title']} (Min: {table['aggs']['min_price']}€ | Max: {table['aggs']['max_price']}€)", expanded=False):
-            # Transformation des données en DataFrame
-            data_list = [{
-                "Zone": zone["name"],
-                "Prix (€/m²)": zone["price"]
-            } for zone in table["items"]]
+        df = pd.DataFrame(data_list)
 
-            df = pd.DataFrame(data_list)
-
-            # Affichage du tableau interactif
-            st.dataframe(df, use_container_width=True)
+        # Affichage du tableau interactif
+        st.dataframe(df, use_container_width=True)

61548a5b8322caf663d920fdecf30815302b969b
Nicolas Delplanque <nicolas@delplanque.eu>
2025-03-14 14:19:01 +0100

    feat(prices): separated prices fetching

--- backend/api/v1/database/queries.py
+++ backend/api/v1/database/queries.py
@@ -6,3 +6,12 @@ def list_departments():
 
 def list_regions():
     return "SELECT region_id, name, ST_AsText(geom) FROM regions;"
+
+def list_cities_prices():
+    return "SELECT city_id, name FROM cities;"
+
+def list_departments_prices():
+    return "SELECT department_id, name FROM departments;"
+
+def list_regions_prices():
+    return "SELECT region_id, name FROM regions;"
